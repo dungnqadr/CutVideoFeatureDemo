c590f07415a91c26e08515fb44ca14b35b64914a
HieuNMFrovis <hieunm.frovis@gmail.com>
2025-09-08 09:05:46 +0700

    feat: clicktest

---
+++ app/src/main/java/com/example/cutvideofeaturedemo/test/ClickTest.kt
@@ -0,0 +1,43 @@
+package com.example.cutvideofeaturedemo.test
+
+import android.util.Log
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ClickTest(modifier: Modifier = Modifier) {
+    var counter by remember { mutableStateOf(0) }
+    Scaffold(
+        modifier = Modifier.padding(WindowInsets.systemBars.asPaddingValues())
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier.fillMaxSize().padding(innerPadding),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Button(
+                onClick = {
+                    counter++
+                }
+            ) {
+                Text(text = "Click me")
+            }
+        }
+    }
+    Log.d("DEBUG", counter.toString())
+}
